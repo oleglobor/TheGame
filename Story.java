1b8a3f3bd64a468d88bd7bf76b27e051053333b0
Oleg <olegromanovichlviv@gmail.com>
2016-08-17 11:40:34 +0300

    Add files via upload

---
+++ Story.java
@@ -0,0 +1,63 @@
+
+public class Story {
+
+		void story(){
+	System.out.println("       Історія ВІКІНГА	");
+	System.out.println("	Створено мною");
+	System.out.println("   для себе самого ");
+	System.out.println("   нажміть 1 для старту ");
+		}
+	void start(){
+		System.out.println(" Прокинься Вікінг");
+		System.out.println(" Прокидайся, вже пора");
+		System.out.println(" ");
+		System.out.println(" - Га? Що?");
+		System.out.println(" ");
+		System.out.println(" - Чортяки б вхопили це все, знову сон.");
+		System.out.println(" ");
+		System.out.println(" Почувся голос збоку");
+		System.out.println(" ");
+		System.out.println(" - Далі дрихнеш здихлику, давай прокидайся і готуйся, ми майже на місці");
+		System.out.println(" ");
+		System.out.println(" Вікінг находився на Дракарі, він з групою інших побратимів відправилсь на розбій");
+		System.out.println(" ");
+		System.out.println(" Наш герой вмився, витер собі лице");
+		System.out.println(" Витягнув шолом");
+		System.out.println(" І зупинився на зброї");
+		System.out.println(" ");
+		System.out.println(" Яку обрати?");
+		System.out.println(" 1 - молот ");
+		System.out.println(" 2 - сокира ");
+		System.out.println(" 3 - меч ");
+	}	
+		void ship(){
+		System.out.println(" Попереду виднілась земля з портовим містом");
+		System.out.println(" ");
+		System.out.println("- Готуйтеся здихлики, нас чекає бій");
+		System.out.println(" ");
+		System.out.println(" Дракар загудів");
+		System.out.println(" - Аррррр ");
+		System.out.println(" - ЗА ОДІНА");
+		System.out.println(" - ЗА ВАЛГАЛЛЛУ");
+		System.out.println(" ");
+		System.out.println(" 1. За ТОРА");
+		System.out.println(" 2. СМЕРТЬ");
+		System.out.println(" 3. КРОВІ ДЛЯ НАШИХ БОГІВ");
+		System.out.println(" 4. Мовчанка ");
+	}
+	
+		void fight(){
+			System.out.println(" Дракар наблизився до порту ");
+			System.out.println("  ");
+		
+		
+		}
+		
+		
+		
+		}
+		
+
+
+	
+
