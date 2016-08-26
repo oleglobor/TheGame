4b7a67b40580128107bbede679277c4a83db3730
Oleg <olegromanovichlviv@gmail.com>
2016-08-26 20:14:06 +0300

    Update Story.java

--- Story.java
+++ Story.java
@@ -1,61 +1,81 @@
 
 public class Story {
 
-		void story(){
-	System.out.println("       Історія ВІКІНГА	");
-	System.out.println("	Створено мною");
-	System.out.println("   для себе самого ");
-	System.out.println("   нажміть 1 для старту ");
+	 public void story() throws InterruptedException {
+		 System.out.println("");
+		String[] text = { 
+				"\tІсторія ВІКІНГА\t", 
+				"\tСтворено мною\t", 
+				"\tдля себе самого\t",
+				"     нажміть 1 для старту\t" };
+		for (int i = 0; i < text.length; i++) {
+			System.out.println(text[i]);
+			Thread.sleep(1000);
 		}
-	void start(){
-		System.out.println(" Прокинься Вікінг");
-		System.out.println(" Прокидайся, вже пора");
-		System.out.println(" ");
-		System.out.println(" - Га? Що?");
-		System.out.println(" ");
-		System.out.println(" - Чортяки б вхопили це все, знову сон.");
-		System.out.println(" ");
-		System.out.println(" Почувся голос збоку");
-		System.out.println(" ");
-		System.out.println(" - Далі дрихнеш здихлику, давай прокидайся і готуйся, ми майже на місці");
-		System.out.println(" ");
-		System.out.println(" Вікінг находився на Дракарі, він з групою інших побратимів відправилсь на розбій");
-		System.out.println(" ");
-		System.out.println(" Наш герой вмився, витер собі лице");
-		System.out.println(" Витягнув шолом");
-		System.out.println(" І зупинився на зброї");
-		System.out.println(" ");
-		System.out.println(" Яку обрати?");
-		System.out.println(" 1 - молот ");
-		System.out.println(" 2 - сокира ");
-		System.out.println(" 3 - меч ");
-	}	
-		void ship(){
-		System.out.println(" Попереду виднілась земля з портовим містом");
-		System.out.println(" ");
-		System.out.println("- Готуйтеся здихлики, нас чекає бій");
-		System.out.println(" ");
-		System.out.println(" Дракар загудів");
-		System.out.println(" - Аррррр ");
-		System.out.println(" - ЗА ОДІНА");
-		System.out.println(" - ЗА ВАЛГАЛЛЛУ");
-		System.out.println(" ");
-		System.out.println(" 1. За ТОРА");
-		System.out.println(" 2. СМЕРТЬ");
-		System.out.println(" 3. КРОВІ ДЛЯ НАШИХ БОГІВ");
-		System.out.println(" 4. Мовчанка ");
+	}
+
+	   public void start() throws InterruptedException {
+		String[] text = { 
+				"\tПрокинься Вікінг\t", 
+				"\tПрокидайся, вже пора\t", 
+				"\t- Га? Що?", 
+				"\t- Чортяки б вхопили це все, знову сон.\t",
+				"\tПочувся голос збоку", 
+				"\t- Далі дрихнеш здихлику, давай прокидайся і готуйся, ми майже на місці\t",
+				"\tВікінг находився на Дракарі, він з групою інших побратимів відправилсь на розбій\t",
+				"\tНаш герой вмився, витер собі лице\t",
+				"\tВитягнув шолом\t",
+				"\tІ зупинився на зброї\t",
+				"\tЯку обрати?\t",
+				"\t1 - молот \t",
+				"\t2 - сокира\t",
+				"\t3 - меч\t"
+		};
+		
+		     for (int i = 0; i < text.length; i++) {
+			   System.out.println(text[i]);
+			     Thread.sleep(2000);
+
+		} 
 	}
 	
+	   public void ship() throws InterruptedException {
+		   String[] text = { 
+				   "\tПопереду виднілась земля з портовим містом\t",
+				   " ",
+				   "\t- Готуйтеся здихлики, нас чекає бій",
+				   " ",
+				   "\tДракар загудів",
+				   "\t- Аррррр ",
+				   "\t- ЗА ОДІНА",
+				   " - ЗА ВАЛГАЛЛЛУ",
+				   " ",
+				   "\t1. За ТОРА",
+				   "\t2. СМЕРТЬ",
+				   "\t3. КРОВІ ДЛЯ НАШИХ БОГІВ",
+				   "\t4. Мовчанка "
+		   			};
+		     for (int i = 0; i < text.length; i++) {
+			   System.out.println(text[i]);
+			     Thread.sleep(2000);
+		     }
+	   	}
+	   
 		void fight(){
 			System.out.println(" Дракар наблизився до порту ");
 			System.out.println("  ");
 		
 		
 		}
-		
-		
-		
+
+
+			// TODO Auto-generated method stub
+			
 		}
+		
+		
+		
+		
 		
 
 
